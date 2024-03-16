00256393bc3c7411ca0f1bbe3a81b964800287e4
Tim <timvan0118@gmail.com>
2024-03-16 16:22:48 -0500

    wtf

--- lib/actions/form.action.ts
+++ lib/actions/form.action.ts
@@ -64,7 +64,7 @@ export async function createArticle(seed?: string) {
   try {
     const parsed = JSON.parse(article)
     addArticles([parsed])
-  } catch (err) {
-    console.log(err)
+  } catch (error) {
+    console.log(error)
   }
 }
