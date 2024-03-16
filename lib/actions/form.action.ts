8c3acad8ba4dac1d28a84499890a94847d59128f
Tim <timvan0118@gmail.com>
2024-03-16 16:20:50 -0500

    Update

--- lib/actions/form.action.ts
+++ lib/actions/form.action.ts
@@ -64,7 +64,7 @@ export async function createArticle(seed?: string) {
   try {
     const parsed = JSON.parse(article)
     addArticles([parsed])
-  } catch (error) {
-    console.log(error)
+  } catch (err) {
+    console.log(err)
   }
 }
